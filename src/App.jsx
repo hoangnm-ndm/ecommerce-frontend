4cb0acb6abefdd5368b2fe0a82ee6fc8b876d34f
hoangnm-ndm <hoangnm.ndm@gmail.com>
2025-02-10 10:13:16 +0700

    source base

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Button } from "@mui/material";
+
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<h1>Welcome to Material UI with Vite! 🚀</h1>
+			<Button variant="contained" color="primary">
+				Click Me
+			</Button>
+		</>
+	);
+}
+
+export default App;
